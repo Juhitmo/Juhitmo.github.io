a33394a9aadd4e08e09248fe2f09cee877167a8b
Juhitmo <Jonwhitmore13@gmail.com>
2016-09-04 20:58:55 -0500

    Release version 1
    - finalized picture selection
    - improved picture size
    - mobile layouts
    - misc. other improvements

--- JW_JS.js
+++ JW_JS.js
@@ -1,4 +1,20 @@
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $(".top");
+    var offset = startchange.height();
+
+    $(document).scroll(function () {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset) {
+            $(".nav-menu").css('color', '#660000');
+        } else {
+            $('.nav-menu').css('color', 'white');
+        }
+    });
+});
+
 $(".links .nav-menu").on("click", function(e) {
   e.preventDefault();
   $(this).parent().toggleClass("nav-open");
+
 });
